08d9470e6bdaef63eb49ea6fd84511b068d187c0
felix <felixeisenmenger@gmx.net>
2019-09-01 09:10:12 +0200

    [UPDATE] add comment

--- exercise49.py
+++ exercise49.py
@@ -33,25 +33,27 @@ class Card:
     def get_value(self) -> CardValue:
         return self._value
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'{self._value.name} of {self._suite.name}'
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f'({self._value.name},{self._suite.name})'
 
-    def __gt__(self, other):
-        return (self._suite.value > other.get_suite.value) or (self._value.value > other.get_value.value)
+    # not really needed but to prepend unforeseen sideeffects its is better to implement it
+    # def __gt__(self, other) -> bool:
+    #     return (self._suite.value > other.get_suite.value) or (self._value.value > other.get_value.value)
 
-    def __lt__(self, other):
+    def __lt__(self, other) -> bool:
         return (self._suite.value < other.get_suite.value) or (self._value.value < other.get_value.value)
 
-    def __eq__(self, other):
+    def __eq__(self, other) -> bool:
         return (self._suite.value == other.get_suite.value) and (self._value.value == other.get_value.value)
 
-    def __ne__(self, other):
-        return (self._suite.value != other.get_suite.value) and (self._value.value != other.get_value.value)
+    # not really needed but to prepend unforeseen sideeffects its is better to implement it
+    # def __ne__(self, other) -> bool:
+    #     return (self._suite.value != other.get_suite.value) and (self._value.value != other.get_value.value)
 
-    def __getitem__(self, item):
+    def __getitem__(self, item) -> Enum:
         if item == 0:
             return self._suite.value
         else:
